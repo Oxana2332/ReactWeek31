254799f01370e7ea1007d76f2ea519fdc1b9600e
Oxana2332 <far_north@ro.ru>
2023-02-10 14:34:14 +0300

    - прозрачность в header, footer

--- flashcards/src/Pages/Home.jsx
+++ flashcards/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 import WordItem from '../components/WordItem/WordItem.jsx'
 import AddNew from '../components/AddNew/AddNew.jsx'
@@ -9,37 +9,37 @@ import '../style/variables.css'
 
 function Home() {
 
-const [words, setWords] = useState([]);
+    const [words, setWords] = useState([]);
 
-useEffect(() => {
-    setWords(data)
-}, [])
+    useEffect(() => {
+        setWords(data)
+    }, [])
 
-function editWordItem(english, transcription, russian, tags, id){
-const resultEditWords = data.map(item=>{
-    if (item.id === id){
-        item.english = english
-        item.transcription = transcription
-        item.russian = russian
-        item.tags = tags
-        return item
+    function editWordItem(english, transcription, russian, tags, id) {
+        const resultEditWords = data.map(item => {
+            if (item.id === id) {
+                item.english = english
+                item.transcription = transcription
+                item.russian = russian
+                item.tags = tags
+                return item
+            }
+            return item
+        })
     }
-    return item
-})
-}
 
     return (
         <div className='dictionary'>
             <div className='dictionary_count'>
-            <div className='dictionary_title' type="text" value='english'>english</div>
-            <div className='dictionary_title' type="text" value='transcription'>transcription</div>
-            <div className='dictionary_title' type="text" value='russian'>russian</div>
-            <div className='dictionary_title' type="text" value='topic'>topic</div>
+                <div className='dictionary_title' type="text" value='english'>english</div>
+                <div className='dictionary_title' type="text" value='transcription'>transcription</div>
+                <div className='dictionary_title' type="text" value='russian'>russian</div>
+                <div className='dictionary_title' type="text" value='topic'>topic</div>
             </div>
-            <AddNew/>
+            <AddNew />
             {
-                words.map((item)=>(
-                    <WordItem key={item.id} item={{...item}} editWordItem={editWordItem} />
+                words.map((item) => (
+                    <WordItem key={item.id} item={{ ...item }} editWordItem={editWordItem} />
                 )
                 )
             }
